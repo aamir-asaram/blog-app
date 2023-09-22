02950f1cbb583490c52a86ab2b68b44919326a67
Aamir <aamirasaram2107@gmail.com>
2023-09-22 07:43:35 +0200

    Add validations and user spec tests

---
+++ app/models/post.rb
@@ -0,0 +1,21 @@
+class Post < ApplicationRecord
+  belongs_to :author, class_name: 'User'
+  has_many :comments
+  has_many :likes
+
+  validates :title, presence: true, length: { maximum: 250 }
+  validates :comments_counter, numericality: { greater_than_or_equal_to: 0 }
+  validates :likes_counter, numericality: { greater_than_or_equal_to: 0 }
+
+  def recent_comments
+    comments.order(created_at: :desc).limit(5)
+  end
+
+  after_save :increment_posts_counter
+
+  private
+
+  def increment_posts_counter
+    author.increment!(:posts_counter)
+  end
+end
