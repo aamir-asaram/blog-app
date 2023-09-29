0eb1ea17e3eebe3a2358db5ee03b51ec37d62579
Aamir <aamirasaram2107@gmail.com>
2023-09-29 13:06:18 +0200

    Add new comment functionality

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   root to: 'users#index'
   resources :users, only: [:index, :show] do
-    resources :posts, only: [:index, :new, :create, :show] do
+    resources :posts, only: [:index, :show, :new, :create] do
       resources :comments, only: [:new, :create]
       resources :likes, only: [:create]
     end
