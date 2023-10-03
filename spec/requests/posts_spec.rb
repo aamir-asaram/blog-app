78a5d2a3ad607096a3904e36f1f0321c48c31679
Aamir <aamirasaram2107@gmail.com>
2023-10-03 09:36:57 +0200

    Add user index spec

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -3,7 +3,9 @@ require 'rails_helper'
 RSpec.describe 'Posts', type: :request do
   describe 'GET users/:user_id/posts' do
     before :each do
-      get '/users/1/posts'
+      @user = User.create(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.')
+      @post = Post.create(title: 'My post', text: 'This is my first post', author: @user)
+      get user_posts_path(@user.id)
     end
 
     it 'returns http success' do
@@ -13,15 +15,13 @@ RSpec.describe 'Posts', type: :request do
     it 'renders the index template' do
       expect(response).to render_template('index')
     end
-
-    it 'renders the correct text' do
-      expect(response.body).to include('List of posts')
-    end
   end
 
   describe 'GET users/:user_id/posts/:id' do
     before :each do
-      get '/users/1/posts/1'
+      @user = User.create(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.')
+      @post = Post.create(title: 'My post', text: 'This is my first post', author: @user)
+      get user_post_path(@user.id, @post.id)
     end
 
     it 'returns http success' do
@@ -30,10 +30,6 @@ RSpec.describe 'Posts', type: :request do
 
     it 'renders the show template' do
       expect(response).to render_template('show')
-    end
-
-    it 'renders the correct text' do
-      expect(response.body).to include('Individual post')
     end
   end
 end
