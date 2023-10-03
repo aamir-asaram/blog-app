daea85ab16ce7b59b5e4cd80c553d5a216719e23
Aamir <aamirasaram2107@gmail.com>
2023-10-03 22:35:47 +0200

    Setup email confirmation and password resetting

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -43,6 +43,16 @@ Rails.application.configure do
 
   # set up the default URL options for the Devise mailer in each environment.
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: "localhost:3000",
+    user_name: "thefailedphotographer.com",
+    password: "aamir2107",
+    authentication: "plain",
+    enable_starttls_auto: true,
+  }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
